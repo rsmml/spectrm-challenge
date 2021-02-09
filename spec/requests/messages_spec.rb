67e39b146395cc133c2490f99ff81c0b3fb80995
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-02-09 17:11:54 +0100

    Tests done

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Messages API', type: :request do
-  it 'Return all instances of Message' do
+  it 'GET all instances of Message' do
     10.times do
       FactoryBot.create(
         :message,
@@ -15,7 +15,7 @@ describe 'Messages API', type: :request do
     expect(JSON.parse(response.body)["messages"].size).to eq(10)
   end
 
-  it 'Return one instance of Message' do
+  it 'GET one instance of Message' do
     message = FactoryBot.create(
       :message,
       body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
@@ -27,7 +27,7 @@ describe 'Messages API', type: :request do
     expect(JSON.parse(response.body).size).to eq(1)
   end
 
-  it 'Creates an instance of Message' do
+  it 'CREATE an instance of Message' do
     message = FactoryBot.create(
       :message,
       body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
@@ -39,7 +39,7 @@ describe 'Messages API', type: :request do
     expect(JSON.parse(response.body)["status"]).to eq("created")
   end
 
-  it 'Updates an instance of Message' do
+  it 'UPDATE an instance of Message' do
     message = FactoryBot.create(
       :message,
       body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
@@ -54,7 +54,7 @@ describe 'Messages API', type: :request do
     expect(JSON.parse(response.body)["message"]["body"]).to match("Updating Body")
   end
 
-  it 'Delete an instance of Message' do
+  it 'DELETE an instance of Message' do
     message = FactoryBot.create(
       :message,
       body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
@@ -72,7 +72,7 @@ describe 'Messages API', type: :request do
 end
 
 describe 'Check Validations', type: :request do
-  it 'Non guessable identifier' do
+  it 'It has a non-guessable identifier' do
     message = FactoryBot.create(
       :message,
       body: 'Testing identifier',
@@ -85,14 +85,56 @@ describe 'Check Validations', type: :request do
     expect(message.id).to match(regex)
   end
 
-  it 'Contains characters from all languages' do
+  it 'It contains characters from all languages' do
     message = FactoryBot.create(
       :message,
-      body: 'Testing identifier',
+      body: 'èéëï ÝÞÆÖáðé ēę̄īį̄óōǫǫ́ łńóśź ش ص ض ط ظ ع غ ЬѢЮꙖѤѦѨ 漢字文  ç è é ê î ô ã, õ, â, ê, ô, á,',
       counter: 0
     )
     get "/api/v1/messages/#{message.id}"
 
     expect(response).to have_http_status(:success)
+    regex = /\A[\A\-]*/
+    expect(message.id).to match(regex)
+  end
+
+  it 'It does not support "<html />" tags' do
+    message = Message.new
+    message.body = '<p>Testing</p>'
+    message.validate
+
+    expect(message.errors[:body]).to include("No HTML <tags />")
+  end
+
+  it 'It has a maximum of 255 chars' do
+    message = Message.new
+    message.body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    message.validate
+
+    expect(message.errors[:body]).to include("is too long (maximum is 255 characters)")
+  end
+
+  it 'It allows e-mails and links' do
+    message = FactoryBot.create(
+      :message,
+      body: "testing@rails.com https://www.lewagon.com/",
+      counter: 0
+    )
+    get "/api/v1/messages/#{message.id}"
+
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'It has an internal counter' do
+    message = FactoryBot.create(
+      :message,
+      body: "testing@rails.com https://www.lewagon.com/",
+      counter: 0
+    )
+    get "/api/v1/messages/#{message.id}"
+
+    expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body)["message"]["counter"].nil?).to eq(false)
+    expect(JSON.parse(response.body)["message"]["counter"]).to eq(1)
   end
 end
